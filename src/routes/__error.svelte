dc2704175f84a2de2a1a577734a6fce4504953f6
jordy248 <jordy@jordynelson.io>
2022-07-12 03:26:03 -0400

    add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+  export function load({error, status}) {
+
+    return {
+      props: {
+        error,
+        status
+      }
+    }
+  }
+</script>
+
+<script>
+  export let error;
+  export let status;
+</script>
+
+<h1>Something Happened</h1>
+
+<h3>error {status}: {error.message}</h3>
