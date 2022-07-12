b160a0f5270a9a2b2456adf67ef12edf1bd68d7c
jordy248 <jordy@jordynelson.io>
2022-07-12 09:41:05 -0400

    add about placeholder

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,5 +1,6 @@
-
 <div class="w-full min-h-screen	text-left margin-auto">
-  <h1 class="text-center text-3xl py-3 sm:text-4xl sm:py-4">Hey I'm the about page</h1>
+    <h1 class="text-center text-3xl py-3 sm:text-4xl sm:py-4">Hey I'm the about page</h1>
+  <p>
+    Have you heard the one about the elevator? It works on so many levels.
+  </p>
 </div>
-
