9e238a3675e35d8646fab7748a11b3f95ca174d9
jordy248 <jordy@jordynelson.io>
2022-07-12 03:26:32 -0400

    update about

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,1 +1,5 @@
-<h1>Hey I'm the about page</h1>
+
+<div class="w-full min-h-screen	text-left margin-auto">
+  <h1 class="text-center text-3xl py-3 sm:text-4xl sm:py-4">Hey I'm the about page</h1>
+</div>
+
