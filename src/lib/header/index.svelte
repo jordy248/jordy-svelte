93cc515da0fac18d499520fe45aedff7785ab000
jordy248 <jordy@jordynelson.io>
2022-11-01 19:32:23 -0400

    toggle menu state on nav clicks

--- src/lib/header/index.svelte
+++ src/lib/header/index.svelte
@@ -15,15 +15,12 @@
 	];
 
 	function expandHeader() {
-		console.log('expandHeader()');
-		// const header = document.querySelector('header');
-		// header.classList.toggle('expanded');
 		document.body.classList.toggle('nav-expanded');
 	}
 </script>
 
 <header class="flex flex-col sm:flex-row justify-center items-center sm:sticky top-0 z-[2147483647] w-full px-8 py-4 border-b border-zinc-600 brightness-90 backdrop-blur-md text-zinc-50 transition-all duration-200">
-	<a href="/" class="logo no-underline transition-all duration-200 delay-300">
+	<a href="/" class="logo no-underline transition-all duration-200 delay-300" on:click={expandHeader}>
 		<div
 		class="[flex: 2 1 auto] flex flex-row justify-center items-center text-center text-5xl font-Neonderthaw"
 	>
@@ -33,14 +30,7 @@
 	</a>
 
 	<div class="sm:invisible sm:hidden absolute top-[0] right-[0] my-2 mx-2" on:click={expandHeader}>
-		<!-- <button class="hamburger">
-			<div class="hamburger-slice">-</div>
-			<div class="hamburger-slice">-</div>
-			<div class="hamburger-slice">-</div>
-		</button> -->
 		<svg class="hamburger" viewBox="0 0 40 40" width="40" height="40" xmlns="http://www.w3.org/2000/svg" stroke="none" fill="none">
-			<!-- <circle cx="50" cy="50" r="40" />
-			<circle cx="150" cy="50" r="4" /> -->
 			<rect x="0" y="18" height="4" class="hamburger--slice hamburger--mid"></rect>
 			<rect x="0" y="10" height="4" class="hamburger--slice hamburger--top"></rect>
 			<rect x="0" y="26" height="4" class="hamburger--slice hamburger--bottom"></rect>
@@ -52,6 +42,7 @@
 			<a
 				href={headerLink.link}
 				class="block [flex: 1 1 100%] sm:[flex: 1 1 auto] px-4 py-2 my-2 sm:my-2 font-medium hover:glow-blue hover:text-sky-200"
+				on:click={expandHeader}
 				>{headerLink.text}</a
 			>
 		{/each}
