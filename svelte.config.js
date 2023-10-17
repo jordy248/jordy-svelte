78ff5d281c16fdb92a3c55eeb02eebede94f86ba
jordy248 <jordy@jordynelson.io>
2023-10-17 03:14:17 -0400

    switch to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,16 @@ import preprocess from "svelte-preprocess";
 import preprocessReact from "svelte-preprocess-react/preprocessReact";
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
-// import node from "@sveltejs/adapter-node";
-import adapter from '@sveltejs/adapter-auto';
+import node from "@sveltejs/adapter-node";
+// import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
 	kit: {
-		// adapter: node(), // for render
-		adapter: adapter(), // for vercel, netlify
+		adapter: node(), // for render
+		// adapter: adapter(), // for vercel, netlify
 
 		// Override http methods in the Todo forms
 		methodOverride: {
